a5ed9b896a60838626a09f3d7fe6e78e38a8c676
KirihimeNatsuki <enzo.lemarchand@ynov.com>
2020-02-29 17:46:09 +0100

    3 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
@@ -9,17 +10,21 @@ import { faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope as farEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faFacebookF, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { BlogComponent } from './blog/blog.component';
+import { ProfilComponent } from './profil/profil.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: 'main', component: MainComponent },
-
+  { path: 'blog', component: BlogComponent },
+  { path: 'profil', component: ProfilComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '', component: MainComponent}
 
 ];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, FontAwesomeModule, RouterModule.forRoot(routes) ],
-  declarations: [ AppComponent, MainComponent, BlogComponent ],
+  imports:      [ BrowserModule, FormsModule, FontAwesomeModule, NgbModule, RouterModule.forRoot(routes) ],
+  declarations: [ AppComponent, MainComponent, BlogComponent, ProfilComponent, ContactComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { 
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'mainComp',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css', './blog/blog.component.css']
+  styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
 
