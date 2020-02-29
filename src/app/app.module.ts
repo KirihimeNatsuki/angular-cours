019042e7c1d9908bb996320b20eab439af2fcc7d
KirihimeNatsuki <enzo.lemarchand@ynov.com>
2020-02-29 14:50:18 +0100

    6 files modified, 5 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { MainComponent } from './main/main.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope as farEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faFacebookF, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { BlogComponent } from './blog/blog.component';
+
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+
+  { path: '', component: MainComponent}
+
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, FontAwesomeModule, RouterModule.forRoot(routes) ],
+  declarations: [ AppComponent, MainComponent, BlogComponent ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule { 
+    constructor(private library: FaIconLibrary) {
+    library.addIcons(faPhoneAlt, faEnvelope, farEnvelope, faFacebookF, faGithub, faLinkedinIn);
+  }
+}
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mainComp',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css', './blog/blog.component.css']
+})
+export class MainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
