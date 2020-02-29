63c694b7970feda698d37ee34dcd03adebff3c3d
KirihimeNatsuki <enzo.lemarchand@ynov.com>
2020-02-29 22:11:38 +0100

    4 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneAlt, faEnvelope, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope as farEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faFacebookF, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { BlogComponent } from './blog/blog.component';
@@ -29,6 +29,6 @@ const routes: Routes = [
 })
 export class AppModule { 
     constructor(private library: FaIconLibrary) {
-    library.addIcons(faPhoneAlt, faEnvelope, farEnvelope, faFacebookF, faGithub, faLinkedinIn);
+    library.addIcons(faPhoneAlt, faEnvelope, farEnvelope, faFacebookF, faGithub, faLinkedinIn, faChevronRight);
   }
 }
